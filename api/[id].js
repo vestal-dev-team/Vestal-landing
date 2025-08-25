f0e174e18ae862df4969904c451d3dea01e2bb7b
Alberto Saiz <albertosaiz16@gmail.com>
2025-08-26 00:41:52 +0200

    Septimo hotfix de redirect

--- api/[id].js
+++ api/[id].js
@@ -20,7 +20,7 @@ export default function handler(req, res) {
     });
   }
 
-  const code = `vest${id}`;
+  const code = `${id}`;
   const deepLink = `vestal://vest/${encodeURIComponent(code)}`;
 
   const isAndroid = /android/.test(ua);
